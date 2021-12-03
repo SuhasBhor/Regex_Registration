eb7d191cc6a8dd6e312744d055cc6ac7d2516b09
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2021-12-03 19:13:09 +0530

    Check User Details Using Junit Parameterized

--- src/com/test/TestClass.java
+++ src/com/test/TestClass.java
@@ -55,22 +55,35 @@ public class TestClass {
 	@Parameters(name="inputs()")
 	public static Collection inputs() {
 		return Arrays.asList(new Object[][] {
-			{"abc@yahoo.com", true},
-			{"abc-100@yahoo.com", true},
-			{"abc.100@yahoo.com", true},
-			{"abc111@abc.com", true},
-			{"abc-100@abc.net", true},
-			{"abc.100@abc.com.au", true},
-			{"abc@1.com", true},
-			{"abc@gmail.com.com", true},
-			{"abc+100@gmail.com", true},
+			{"abc@yahoo.com" , true},
+			{"abc-100@yahoo.com" , true},
+			{"abc.100@yahoo.com" , true},
+			{"abc111@abc.com" , true},
+			{"abc-100@abc.net" , true},
+			{"abc.100@abc.com.au" , true},
+			{"abc@1.com" , true},
+			{"abc@gmail.com.com" , true},
+			{"abc+100@gmail.com.com" , true},
+			{"abc+100@gmail.com" , true},
+			{"abc@.com.com" , false},
+			{"abc123@gmail.a" , false},
+			{"abc123@.com" , false},
+			{"abc123@.com.com" , false},
+			{".abc@abc.com" , false},
+			{"abc()*@gmail.com" , false},
+			{"abc@%*.com" , false},
+			{"abc..2002@gmail.com" , false},
+			{"abc.@gmail.com" , false},
+			{"abc@abc@gmail.com" , false},
+			{"abc@gmail.com.1a" , false},
+			{"abc@gmail.com.aa.au^$" , false}
 			} );
 	}
 	@Test
 	public void testEmail() {
 		validation = new RegexRegistration();
 		boolean output = validation.validateEmail(input);
-		assertEquals(true , output);
+		assertEquals(expectedResult , output);
 	}
 	/*@Test
 	public void testEmailHappy() {
